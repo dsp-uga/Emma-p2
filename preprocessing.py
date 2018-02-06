5fc76e1039fdf059999d07272e493b0a09f7008b
Prajay Shetty <prajayshetty@Prajays-MacBook-Air.local>
2018-02-06 13:00:48 -0500

    Updated preprocessing.py to add ngrams Welcome to Ngramsgit add preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,7 @@ from pyspark.sql import SQLContext
 
 from pyspark.ml.feature import *
 from pyspark import SparkContext
+from pyspark.sql.functions import udf
 import argparse
 
 import requests
@@ -11,6 +12,22 @@ PATH = "https://storage.googleapis.com/uga-dsp/project2/data/bytes"
 
 sc = SparkContext("local[*]",'pyspark tuitorial')
 sqlContext = SQLContext(sc)
+
+
+def merge_col(*x):
+	return list(x)
+
+def ngram_processor(df,n_count=3):
+	cols = list();
+	for i in range(2,n_count+1):
+		cols.append("text_ngram_"+str(i))
+		ngram = NGram(n=i, inputCol="text", outputCol="text_ngram_"+str(i))
+		df = ngram.transform(df)
+
+	merge_udf = udf(merge_col)
+	df = df.select("class_label",merge_udf("text","text_ngram_2","text_ngram_3"))
+	return df
+
 
 def fetch_url(x,path):
 	class_label = x[1][1]
@@ -29,8 +46,7 @@ def open_row(x):
 
 def clean(x):
 	temp = x[0].split(' ')[1:]
-	text = ' '.join(temp)
-	return (text,x[1])
+	return (temp,x[1])
 
 
 parser = argparse.ArgumentParser(description='Welcome to Team Emma.')
@@ -45,7 +61,6 @@ parser.add_argument('--test_y', type=str,
 
 args = parser.parse_args()
 print(args)
-
 rdd_train_x = sc.textFile(args.train_x).zipWithIndex().map(lambda l:(l[1],l[0]));
 rdd_train_y = sc.textFile(args.train_y).zipWithIndex().map(lambda l:(l[1],l[0]));
 rdd_test_x = sc.textFile(args.test_x).zipWithIndex().map(lambda l:(l[1],l[0]));
@@ -56,12 +71,19 @@ rdd_train = rdd_train.flatMap(lambda l :fetch_url(l,PATH)).map(lambda l:clean(l)
 rdd_test = rdd_test.flatMap(lambda l :fetch_url(l,PATH)).map(lambda l:clean(l))
 
 
-print(rdd_train.take(1))
+df_train = sqlContext.createDataFrame(rdd_train,schema=["text","class_label"])
+df_test = sqlContext.createDataFrame(rdd_test,schema=["text","class_label"])
 
-df_train = sqlContext.createDataFrame(rdd_train)
-df_test = sqlContext.createDataFrame(rdd_test)
-print(df_train.show())
-print(df_test.show())
+df_train.cache()
+df_test.cache()
+
+df_train = ngram_processor(df_train,n_count=3)
+df_train.count()
+
+df_test = ngram_processor(df_test,n_count=3)
+df_test.count()
+
+
 
 
 
