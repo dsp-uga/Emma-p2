16236eb8dcaf2e1fd2ced10e6b4f3e9ba00fc6a9
Prajay Shetty <prajayshetty@s172-21-173-h11.paws.uga.edu>
2018-02-08 01:11:03 -0500

    Updated naive bayes received accuracy 0.97% , selected 3 files only due to memory issue and gs protocol not working

--- logistic-regression.py
+++ logistic-regression.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SQLContext
+from pyspark.mllib.classification import *
+from pyspark.mllib.util import MLUtils
 
 from pyspark.ml.feature import *
 from pyspark import SparkContext
@@ -7,7 +9,7 @@ import argparse
 
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.mllib.linalg import SparseVector
-from pyspark.mllib.classification import *
+
 
 import requests
 import os;
@@ -15,7 +17,7 @@ from pyspark.sql.types  import *
 import sys
 PATH = "https://storage.googleapis.com/uga-dsp/project2/data/bytes"
 
-sc = SparkContext("local[*]",'pyspark tuitorial')
+sc = SparkContext()#"local[*]",'pyspark tuitorial'
 sqlContext = SQLContext(sc)
 
 def merge_col(*x):
@@ -27,9 +29,9 @@ def merge_col(*x):
 
 
 def tfidf_processor(df,inputCol="text",outputCol="tfidf_vector"):
-	hashingTF = HashingTF(inputCol=inputCol, outputCol="raw_features", numFeatures=300)
+	hashingTF = HashingTF(inputCol=inputCol, outputCol="raw_features", numFeatures=400)
 	tf = hashingTF.transform(df)
-	idf = IDF(inputCol="raw_features", outputCol=outputCol,minDocFreq=2)
+	idf = IDF(inputCol="raw_features", outputCol=outputCol,minDocFreq=3)
 	idfModel = idf.fit(tf)
 	df = idfModel.transform(tf)
 	return df.drop(col("raw_features"));
@@ -87,29 +89,30 @@ def clean(x):
 
 
 parser = argparse.ArgumentParser(description='Welcome to Team Emma.')
-parser.add_argument('--train_x', type=str,
+parser.add_argument('-a','--train_x', type=str,
                     help='training x set')
-parser.add_argument( '--train_y' ,help='training y set')
-parser.add_argument('--test_x', type=str,
+parser.add_argument( '-b','--train_y' ,help='training y set')
+parser.add_argument('-c','--test_x', type=str,
                     help='testing x set')
-parser.add_argument('--test_y', type=str,
+parser.add_argument('-d','--test_y', type=str,
                     help='testing y set')
-parser.add_argument('--path', type=str,
+parser.add_argument('-e','--path', type=str,
                     help='path to folder')
 
-args = parser.parse_args()
-print(args)
-rdd_train_x = sc.textFile(args.train_x).zipWithIndex().map(lambda l:(l[1]-1,l[0]))
-rdd_train_y = sc.textFile(args.train_y).zipWithIndex().map(lambda l:(l[1],l[0]));
-rdd_test_x = sc.textFile(args.test_x).zipWithIndex().map(lambda l:(l[1],l[0]));
-rdd_test_y = sc.textFile(args.test_y).zipWithIndex().map(lambda l:(l[1],l[0]));
+args = vars(parser.parse_args())
+#print(args)
+rdd_train_x = sc.textFile(args['train_x']).zipWithIndex().map(lambda l:(l[1],l[0]))
+rdd_train_y = sc.textFile(args['train_y']).zipWithIndex().map(lambda l:(l[1]-1,l[0]));
+rdd_test_x = sc.textFile(args['test_x']).zipWithIndex().map(lambda l:(l[1],l[0]));
+rdd_test_y = sc.textFile(args['test_y']).zipWithIndex().map(lambda l:(l[1]-1,l[0]));
 rdd_train = rdd_train_x.join(rdd_train_y)
 rdd_test = rdd_test_x.join(rdd_test_y)
 #take 30 due to gc overhead
-rdd_train = rdd_train.flatMap(lambda l :fetch_url(l,args.path)).map(lambda l:clean(l))
+print(rdd_train.take(10))
+rdd_train = rdd_train.flatMap(lambda l :fetch_url(l,args['path'])).map(lambda l:clean(l))
 #rdd_train = sc.parallelize(rdd_train)
 
-rdd_test= rdd_test.flatMap(lambda l :fetch_url(l,args.path)).map(lambda l:clean(l))
+rdd_test= rdd_test.flatMap(lambda l :fetch_url(l,args['path'])).map(lambda l:clean(l))
 #rdd_test = sc.parallelize(rdd_test)
 
 #original dataframe
@@ -117,7 +120,7 @@ rdd_test= rdd_test.flatMap(lambda l :fetch_url(l,args.path)).map(lambda l:clean(
 
 df_train_orignal = sqlContext.createDataFrame(rdd_train,schema=["text","class_label"])
 df_test_orignal = sqlContext.createDataFrame(rdd_test,schema=["text","class_label"])
-df_train_orignal ,df_train_orignal_validate =df_train_orignal.randomSplit([0.7,0.3])
+#df_train_orignal ,df_train_orignal_validate =df_train_orignal.randomSplit([0.7,0.3])
 
 
 df_train_orignal.printSchema()
@@ -148,10 +151,26 @@ df_tfidf_train = tfidf_processor(df_train_orignal,"text","tfidf_vector");
 df_tfidf_test = tfidf_processor(df_test_orignal,"text","tfidf_vector");
 df_tfidf_test = df_tfidf_test.rdd.map(lambda l:LabeledPoint(l[1],l[-1].toArray()))
 df_tfidf_train= df_tfidf_train.rdd.map(lambda l:LabeledPoint(l[1],l[-1].toArray()))
-model =  LogisticRegressionWithLBFGS.train(df_tfidf_train, iterations=100,numClasses=8)
-labelsAndPreds = parsedData.map(lambda p: (p.label, model.predict(p.features)))
-trainErr = labelsAndPreds.filter(lambda lp: lp[0] != lp[1]).count() / float(parsedData.count())
-print("Training Error = " + str(trainErr))
+
+#print(df_tfidf_test.take(20)[-1][-1])
+#df_train_vectorizer.show();
+#df_test_vectorizer.show();
+#df_tfidf_train.show()
+#df_tfidf_test.show()
+
+
+# Split data approximately into training (60%) and test (40%)
+training=df_tfidf_train
+test=df_tfidf_test
+
+# Train a naive Bayes model.
+model = NaiveBayes.train(training, 0.7)
+
+# Make prediction and test accuracy.
+predictionAndLabel = test.map(lambda p: (model.predict(p.features), p.label))
+accuracy = 1.0 * predictionAndLabel.filter(lambda pl: pl[0] == pl[1]).count() / test.count()
+#print(predictionAndLabel.map(lambda x:x[0]).collect())
+print('model accuracy {}'.format(accuracy))
 
 
 #print(df_tfidf_test.take(20)[-1][-1])
